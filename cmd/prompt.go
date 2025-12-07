fe8fb4ab76b7a9f80458f0643e7c6fecdd5e76d3
Hendrik Mans <hendrik@mans.de>
2025-12-07 17:59:36 +0100

    feat: Inline issue types in prompt output
    - Dynamically append issue types from config.yaml to prompt output
    - Remove references to manually checking config.yaml for types
    - Remove static Configuration section from prompt template (now dynamic)

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -17,6 +18,23 @@ var promptCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Print(agentPrompt)
+
+		// Append dynamic issue types section from config
+		if cfg != nil && len(cfg.Types) > 0 {
+			var sb strings.Builder
+			sb.WriteString("\n## Issue Types\n\n")
+			sb.WriteString("This project has the following issue types configured. Always specify a type with `-t` when creating beans:\n\n")
+			for _, t := range cfg.Types {
+				if t.Description != "" {
+					sb.WriteString(fmt.Sprintf("- **%s**: %s\n", t.Name, t.Description))
+				} else {
+					sb.WriteString(fmt.Sprintf("- **%s**\n", t.Name))
+				}
+			}
+			sb.WriteString("\n")
+			fmt.Print(sb.String())
+		}
+
 		return nil
 	},
 }
